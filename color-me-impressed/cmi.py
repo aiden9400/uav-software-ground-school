4f68035769888011a1418d78b99a49dd29a9b3d3
Aiden Shi <shiaiden@gmail.com>
2026-09-20 16:56:16 -0700

    x,y positions for each color

--- color-me-impressed/cmi.py
+++ color-me-impressed/cmi.py
@@ -9,10 +9,21 @@ hsv = cv.cvtColor(img, cv.COLOR_BGR2HSV)
 masks = []
 res = []
 
+def avg_pos(mask):
+    y, x = np.where(mask == 255)
+    if len(x) == 0:
+        return None
+    return int(np.mean(x)), int(np.mean(y))
+
 for i in range(0, 180, 30):
     mask = cv.inRange(hsv, np.array([i, 20, 20]), np.array([i + 30, 255, 255]))
     masks.append(mask)
-    res.append(cv.resize(cv.bitwise_and(img, img, mask=mask), None, fx = 0.2, fy = 0.2))
+    res.append(cv.resize(cv.bitwise_and(img, img, mask=mask), None, fx = 1.0, fy = 1.0))
+
+for i in range(len(masks)):
+    pos = avg_pos(masks[i])
+    if pos is not None:
+        print(f"Average position of hue {i*30} to {i*30 + 30}: {pos}")
 
 out = np.concatenate(res, axis=0)
 cv.imshow("Display window", out)
